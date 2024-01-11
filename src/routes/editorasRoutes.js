0641627fc34c5a6d12148c09812243d68bb8afc9
marcospaulopaiva <marcos.poo@gmail.com>
2024-01-10 22:40:25 -0300

    Aula 4 - Testando uma API.

---
+++ src/routes/editorasRoutes.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+import EditorasController from '../controllers/editorasController.js';
+
+const router = express.Router();
+
+router
+  .get('/editoras', EditorasController.listarEditoras)
+  .get('/editoras/:id', EditorasController.listarEditoraPorId)
+  .get('/editoras/:id/livros', EditorasController.listarLivrosPorEditora)
+  .post('/editoras', EditorasController.cadastrarEditora)
+  .put('/editoras/:id', EditorasController.atualizarEditora)
+  .delete('/editoras/:id', EditorasController.excluirEditora);
+
+export default router;
